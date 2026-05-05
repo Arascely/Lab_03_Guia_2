a1fe8ce0233befec486d05268620439c32311d7f
Arascely <grissel.rodriguez.27@unsch.edu.pe>
2026-05-05 16:03:42 -0500

    para desarrollar

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+      }
+    },
+    rules: {
+      "eqeqeq": "error",
+      "no-var": "warn",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "prefer-const": "warn",
+    }
+  }
+];
+
